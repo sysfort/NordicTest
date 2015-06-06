b5953594a809803d1bd94c4b91d8fc9d4ccc89d0
Ravi P <android.dreamscapemedia@gmail.com>
2015-06-06 17:47:04 +0530

    NordicSampleCodeForBLE_nRF51822
    This is code which enables bluethooth paired Device and

---
+++ Nordic_nRF51822/pca10028/s110/arm5/_build/app_timer_appsh.d
@@ -0,0 +1,11 @@
+.\_build\app_timer_appsh.o: RTE\nRF_Libraries\nRF51422_xxAC\app_timer_appsh.c
+.\_build\app_timer_appsh.o: RTE\nRF_Libraries\nRF51422_xxAC\app_timer_appsh.h
+.\_build\app_timer_appsh.o: RTE\nRF_Libraries\nRF51422_xxAC\app_timer.h
+.\_build\app_timer_appsh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\app_timer_appsh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\_build\app_timer_appsh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\_build\app_timer_appsh.o: RTE\nRF_Libraries\nRF51422_xxAC\app_error.h
+.\_build\app_timer_appsh.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_SoftDevice_S110\8.0.1\headers\nrf_error.h
+.\_build\app_timer_appsh.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Libraries\3.0.0\util\app_util.h
+.\_build\app_timer_appsh.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\compiler_abstraction.h
+.\_build\app_timer_appsh.o: RTE\nRF_Libraries\nRF51422_xxAC\app_scheduler.h
---
+++ Nordic_nRF51822/pca10028/s110/arm5/_build/ble_srv_common.d
@@ -0,0 +1,15 @@
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_BLE\3.0.0\common\ble_srv_common.c
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_BLE\3.0.0\common\ble_srv_common.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_SoftDevice_S110\8.0.1\headers\ble_types.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Libraries\3.0.0\util\app_util.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\7.2.1\Device\Include\compiler_abstraction.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_SoftDevice_S110\8.0.1\headers\ble_gap.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_SoftDevice_S110\8.0.1\headers\ble_ranges.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_SoftDevice_S110\8.0.1\headers\nrf_svc.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_SoftDevice_S110\8.0.1\headers\ble_gatt.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Libraries\3.0.0\util\nordic_common.h
+.\_build\ble_srv_common.o: C:\Users\pc\Documents1\ble_peripheral\Nordic_nRF51822\pca10028\s110\arm5\RTE\nRF_Libraries\nRF51422_xxAC\app_error.h
+.\_build\ble_srv_common.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_SoftDevice_S110\8.0.1\headers\nrf_error.h
